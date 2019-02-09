cbf5cb7e50703f734597b1a4368615daa98036b9
Vipul Katiyar <vipul.katiyar1997@gmail.com>
2019-02-09 22:32:38 +0530

    updating files

---
+++ editor/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class User(models.Model):
+    user = models.CharField(max_length=250)
+    email = models.CharField(max_length=250)
+    password = models.CharField(max_length=250)
+    DOB = models.CharField(max_length=250)
+
